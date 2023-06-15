b37656ce8e770b124752bf45e0e1c10f3de8cc36
taeginote <ab315601@naver.com>
2023-06-16 04:00:09 +0900

    💄Design : Icon 추가

--- src/Components/Icons/Icons.js
+++ src/Components/Icons/Icons.js
@@ -1,6 +1,16 @@
 import { CiSearch } from 'react-icons/ci'
-import { AiOutlineClose, AiOutlineHeart, AiFillHeart } from 'react-icons/ai'
-import { BsFillPersonFill, BsChat } from 'react-icons/bs'
+import {
+	AiOutlineClose,
+	AiOutlineHeart,
+	AiFillHeart,
+	AiOutlineMail,
+	AiOutlineLock,
+} from 'react-icons/ai'
+import { BsFillPersonFill, BsChat, BsPhone } from 'react-icons/bs'
+import { BiUser } from 'react-icons/bi'
+import { MdOutlineToday } from 'react-icons/md'
+import { RiUser5Line } from 'react-icons/ri'
+
 export const Search_Icon = ({ color, position }) => (
 	<CiSearch color={color} position={position} />
 ) // 검색
@@ -20,3 +30,15 @@ export const NotFillHeart_Icon = ({ size, onClick }) => (
 export const Person_Icon = ({ size }) => <BsFillPersonFill size={size} /> // 사람
 
 export const Chat_Icon = ({ size }) => <BsChat size={size} /> // 채팅
+
+export const Email_Icon = ({ size }) => <AiOutlineMail size={size} /> //이메일
+
+export const Lock_Icon = ({ size }) => <AiOutlineLock size={size} /> //자물쇠
+
+export const Name_Icon = ({ size }) => <BiUser size={size} /> //인간
+
+export const Phone_Icon = ({ size }) => <BsPhone size={size} /> //핸드폰
+
+export const Date_Icon = ({ size }) => <MdOutlineToday size={size} /> //달력
+
+export const Nickname_Icon = ({ size }) => <RiUser5Line size={size} /> //달력
