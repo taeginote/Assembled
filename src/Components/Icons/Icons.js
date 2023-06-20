5aef2c51be1ed88751b7cea70b22fb57f595969d
taeginote <ab315601@naver.com>
2023-06-20 19:13:45 +0900

    📝Memo : Icons 추가

--- src/Components/Icons/Icons.js
+++ src/Components/Icons/Icons.js
@@ -5,11 +5,21 @@ import {
 	AiFillHeart,
 	AiOutlineMail,
 	AiOutlineLock,
+	AiFillBank,
 } from 'react-icons/ai'
-import { BsFillPersonFill, BsChat, BsPhone } from 'react-icons/bs'
+import {
+	BsFillPersonFill,
+	BsChat,
+	BsPhone,
+	BsCaretDownFill,
+	BsFileEarmarkPerson,
+} from 'react-icons/bs'
 import { BiUser } from 'react-icons/bi'
 import { MdOutlineToday } from 'react-icons/md'
-import { RiUser5Line } from 'react-icons/ri'
+import { RiUser5Line, RiComputerLine } from 'react-icons/ri'
+
+import { FiBook } from 'react-icons/fi'
+import { IoIosAirplane } from 'react-icons/io'
 
 export const Search_Icon = ({ color, position }) => (
 	<CiSearch color={color} position={position} />
@@ -46,3 +56,17 @@ export const Nickname_Icon = ({ size }) => <RiUser5Line size={size} /> //달력
 export const Warning_Icon = ({ size }) => (
 	<CiWarning size={size} color="#FB9B00" />
 ) //경고
+
+export const Down_Icon = ({ size }) => (
+	<BsCaretDownFill size={size} color="#FB9B00" />
+)
+
+export const Computer_Icon = () => <RiComputerLine size={'30'} /> //컴퓨터
+
+export const Book_Icon = () => <FiBook size={'30'} /> //독서
+
+export const People_Icon = () => <BsFileEarmarkPerson size={'30'} /> //면접
+
+export const Club_Icon = () => <AiFillBank size={'30'} /> //동아리
+
+export const Travel_Icon = () => <IoIosAirplane size={'30'} /> //여행
