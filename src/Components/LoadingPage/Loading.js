ef826e7d9d264e2e7b37dd78bf6307c8fbbc0ae7
taeginote <ab315601@naver.com>
2023-06-20 19:14:12 +0900

    ✨Feat : 로딩창 스피너 추가 구현

---
+++ src/Components/LoadingPage/Loading.js
@@ -0,0 +1,46 @@
+import styled from 'styled-components'
+
+function LoadingPage() {
+	return (
+		<S.Wrapper>
+			<img src="/assets/img/LogoTotal.png" />
+		</S.Wrapper>
+	)
+}
+export default LoadingPage
+const Wrapper = styled.div`
+	position: absolute;
+	width: 100vw;
+	height: 100vh;
+	top: 0;
+	left: 0;
+	z-index: 999;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	background-color: white;
+	& > img {
+		width: 40rem;
+		-webkit-animation: spin 2s linear infinite;
+		animation: spin 2s linear infinite;
+		@-webkit-keyframes spin {
+			0% {
+				-webkit-transform: rotate(0deg);
+			}
+			100% {
+				-webkit-transform: rotate(360deg);
+			}
+		}
+
+		@keyframes spin {
+			0% {
+				transform: rotate(0deg);
+			}
+			100% {
+				transform: rotate(360deg);
+			}
+		}
+	}
+`
+const S = { Wrapper }
