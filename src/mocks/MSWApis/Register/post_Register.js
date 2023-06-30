2da967121b7882dda325e16a499f4df3478af192
taeginote <ab315601@naver.com>
2023-07-01 02:37:39 +0900

    ✨Feat : 모임 등록 MSW 오류 해결

--- src/mocks/MSWApis/Register/post_Register.js
+++ src/mocks/MSWApis/Register/post_Register.js
@@ -1,5 +1,6 @@
 import { rest } from 'msw'
 import listData from '../../Data/ListData'
+import CurrentTime from '../../../Utils/CurrentTime'
 
 export const post_Register = [
 	rest.post('/register', (req, res, ctx) => {
@@ -15,16 +16,27 @@ export const post_Register = [
 		} = data
 
 		const realData = {
-			id: Math.floor(Math.random() * 9999) + 1,
-			writer,
-			category: category === 'study' ? '스터디' : '프로젝트',
-			contents,
-			expectedPeriod,
-			personnelNumber,
+			postId: Math.floor(Math.random() * 9999) + 1,
 			title,
+			contents,
+			category,
+			writer,
+			writeDate: CurrentTime(),
+			postStatus: '모집중',
+			hits: '4',
+			likes: '2',
+			personnelNumber,
+			expectedPeriod,
+			profile: {
+				fileFullPath:
+					'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbiQdhWxfSe3uKwTyX4EK_JPrS7wppr6p6FA&usqp=CAU', // 예시) D://project/file
+				originalName: '파일 이름',
+			},
+			commentCount: 0,
+			comments: [],
 		}
 
-		listData.unshift(realData)
+		listData.response.unshift(realData)
 		return res(ctx.status(200), ctx.json())
 	}),
 ]
