3e946563025c33c6b106ea8d78abcd34603a9665
taeginote <ab315601@naver.com>
2023-07-18 23:13:04 +0900

    ✨Feat : baseurl 변경

--- src/Apis/@core.ts
+++ src/Apis/@core.ts
@@ -1,14 +1,14 @@
 import axios from 'axios'
-import TokenService from '../Utils/TokenService'
+// import TokenService from '../Utils/TokenService'
 
 const axiosInstance = axios.create({
-	baseURL: 'http://localhost:3000',
-	// baseURL:
-	// 	'https://port-0-assemble-deploy-temp-20zynm2mlk476ngf.sel4.cloudtype.app',
+	// baseURL: 'http://localhost:3000',
+	baseURL:
+		'https://port-0-assemble-deploy-temp-20zynm2mlk476ngf.sel4.cloudtype.app',
 	withCredentials: true,
-	headers: {
-		Authorization: `Bearer ${TokenService.getAccessToken()}`,
-	},
+	// headers: {
+	// 	Authorization: `Bearer ${TokenService.getAccessToken()}`,
+	// },
 })
 
 export default axiosInstance
