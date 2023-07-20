81352cb178294803c8861398550226c13c7d344d
taeginote <ab315601@naver.com>
2023-07-20 22:16:06 +0900

    ✨Feat : 말풍선 추가

---
+++ src/Components/Ballon/Ballon.tsx
@@ -0,0 +1,35 @@
+import { styled } from 'styled-components'
+import { FlexCenterCSS } from '../../Styles/common'
+import { BallonType } from '../../Types/type'
+
+function Ballon({ text }: BallonType) {
+	return <S.BallonText>{text}</S.BallonText>
+}
+export default Ballon
+
+const BallonText = styled.div`
+	top: -3.5rem;
+	left: -7rem;
+	position: absolute;
+	width: 10rem;
+	height: 3rem;
+	${FlexCenterCSS}
+
+	background: #FCBA5C;
+	color: white;
+	border-radius: 5px;
+	padding: 12px 12.8px;
+	z-index: 10000;
+	&::after {
+		border-top: 10px solid #fcba5c;
+		border-left: 10px solid transparent;
+		border-right: 10px solid transparent;
+		border-bottom: 0px solid transparent;
+		content: '';
+		position: absolute;
+		top: 3rem;
+		left: 7rem;
+	}
+`
+
+const S = { BallonText }
