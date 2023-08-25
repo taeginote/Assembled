747421ff421c39ddd4921e582cc0fad967f62de3
taeginote <ab315601@naver.com>
2023-08-25 20:14:40 +0900

    STYLE : 1Depth로 변경

--- src/Pages/Admin/Components/AdminSideFilterNav.tsx
+++ src/Pages/Admin/Components/AdminSideFilterNav.tsx
@@ -2,7 +2,6 @@ import styled from 'styled-components'
 
 import { useLocation, useNavigate } from 'react-router-dom'
 import { useEffect, useState } from 'react'
-import { SmallDownIcon } from '../../../Icons/Icons'
 import { FlexAlignCSS, FlexColumnCSS } from '../../../Styles/common'
 
 function AdminSideFilterNav() {
@@ -34,37 +33,9 @@ function AdminSideFilterNav() {
 		<S.Wrapper>
 			{myPageFilterList.map((el, idx) => (
 				<>
-					{el.depth === 2 ? (
-						<S.OneDepthBox
-							key={idx}
-							onClick={
-								selectTitle === el.title
-									? () => setSelectTitle(null)
-									: () => setSelectTitle(el.title)
-							}
-						>
-							{el.sort}
-							<div>
-								<SmallDownIcon />
-							</div>
-						</S.OneDepthBox>
-					) : (
-						<S.OneDepthBox key={idx} onClick={() => onClickOneDepth(el.title)}>
-							{el.sort}
-						</S.OneDepthBox>
-					)}
-					<>
-						{el.title === selectTitle &&
-							el.children.map((two, idx) => (
-								<S.TwoDepthBox
-									key={idx}
-									onClick={() => navigate(`/myPage${two.url}`)}
-									$state={`/myPage${two.url}` === pathname}
-								>
-									{two.name}
-								</S.TwoDepthBox>
-							))}
-					</>
+					<S.OneDepthBox key={idx} onClick={() => onClickOneDepth(el.title)}>
+						{el.sort}
+					</S.OneDepthBox>
 				</>
 			))}
 		</S.Wrapper>
@@ -91,29 +62,5 @@ const OneDepthBox = styled.div`
 	${FlexAlignCSS}
 	justify-content: space-between;
 `
-const TwoDepthBox = styled.div<{ $state: boolean }>`
-	width: 140%;
-	min-height: 4.3rem;
-	@keyframes dropdown {
-		0% {
-			transform: translateY(-100%);
-		}
-		100% {
-			transform: translateY(0);
-		}
-	}
-	animation: dropdown 0.4s ease;
-	cursor: pointer;
-	font-size: ${({ theme }) => theme.FONT_SIZE.xs};
-	${FlexAlignCSS};
-	padding: 0.5rem 0 0.5rem 1.5rem;
-	background-color: ${({ $state }) => ($state ? '#EEE' : '#ffffff')};
-	border: 1px solid ${({ theme }) => theme.COLOR.common.gray[100]};
 
-	border-top: none;
-	&:hover {
-		background-color: #eee;
-	}
-`
-
-const S = { Wrapper, OneDepthBox, TwoDepthBox }
+const S = { Wrapper, OneDepthBox }
