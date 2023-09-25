0d089728d4408d23264d728ad0feda74f49db69a
taeginote <ab315601@naver.com>
2023-09-25 20:51:55 +0900

    STYLE : 모임 활동 페이지 채팅 디자인

--- src/Pages/Activity/Components/ChattingPage.tsx
+++ src/Pages/Activity/Components/ChattingPage.tsx
@@ -1,29 +1,142 @@
 import { styled } from 'styled-components'
+import ProfileImgReturn from '../../../Utils/ProfileImgReturn'
+import { useState } from 'react'
+import { Send_Icon } from '../../../Icons/Icons'
+import { TextareaEventTargetType } from '../../../Types/type'
 
 function ChattingPage() {
+	const testArr: 0[] = Array(20).fill(0)
+
+	const [inputChatting, setInputChatting] = useState<null | string>(null)
+
+	const onSend = () => {
+		if (inputChatting === null || inputChatting!.trim().length === 0) return
+		setInputChatting(null)
+	}
+	const onEnterSend = (e: TextareaEventTargetType) => {
+		if (e.nativeEvent.isComposing) return
+		if (e.key === 'Enter') {
+			e.preventDefault()
+			if (inputChatting === null || inputChatting!.trim().length === 0) return
+		}
+	}
+
 	return (
-		<S.Wrapper>
-			<S.Chatting>
-				<S.Title>채팅</S.Title>
-			</S.Chatting>
-		</S.Wrapper>
+		<S.Chatting>
+			<S.ChattingZone>
+				<S.Intro>
+					<div>채팅을 시작합니다</div>
+				</S.Intro>
+				{testArr.map((el, idx: number) => (
+					<S.ChatList key={idx}>
+						<S.UserImg src={ProfileImgReturn(undefined)} />
+						<S.ChattingContent>안녕하세요~~</S.ChattingContent>
+					</S.ChatList>
+				))}
+			</S.ChattingZone>
+			<S.InputWrapper>
+				<S.InputChatting
+					placeholder={'메세지 쓰기'}
+					$status={inputChatting !== null}
+					onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) =>
+						setInputChatting(e.target.value)
+					}
+					onKeyDown={onEnterSend}
+				/>
+				<S.IconWrapper onClick={onSend}>
+					<Send_Icon />
+				</S.IconWrapper>
+			</S.InputWrapper>
+		</S.Chatting>
 	)
 }
 export default ChattingPage
 
 const Wrapper = styled.div`
 	width: 100%;
-	height: 100%;
+	height: 50%;
+
 	display: flex;
 	justify-content: center;
 `
 const Chatting = styled.div`
 	width: 100%;
+	height: 100%;
 `
-const Title = styled.div`
+
+const ChattingZone = styled.div`
+	font-size: ${({ theme }) => theme.FONT_SIZE.tiny};
 	width: 100%;
-	border-bottom: 1px solid ${({ theme }) => theme.COLOR.common.gray[400]};
-	font-size: ${({ theme }) => theme.FONT_SIZE.small};
-	padding: 1rem 0 1rem 2rem;
+	height: 78vh;
+	overflow: auto;
 `
-const S = { Wrapper, Chatting, Title }
+const Intro = styled.div`
+	width: 100%;
+	display: flex;
+	justify-content: center;
+	text-align: center;
+	& > div {
+		width: 15rem;
+		padding: 0.2rem 0;
+		border-radius: 2rem;
+		margin: 2rem 0;
+		background-color: ${({ theme }) => theme.COLOR.main};
+	}
+`
+const ChatList = styled.div`
+	margin-left: 3rem;
+	margin-bottom: 2rem;
+	display: flex;
+	align-items: center;
+`
+const UserImg = styled.img`
+	width: 4rem;
+	height: 4rem;
+	border-radius: 50%;
+`
+
+const ChattingContent = styled.div`
+	background-color: ${({ theme }) => theme.COLOR.orange};
+	margin-left: 1rem;
+	padding: 0.2rem 1rem;
+	border-radius: 1rem;
+`
+const InputChatting = styled.textarea<{ $status: boolean }>`
+	border: none;
+	border: 2px solid ${({ theme }) => theme.COLOR.common.gray[100]};
+	width: 95%;
+	font-size: ${({ theme }) => theme.FONT_SIZE.tiny};
+	color: ${({ theme, $status }) =>
+		$status ? 'black' : theme.COLOR.common.gray[200]};
+	height: 12.9vh;
+	padding: 2rem;
+	margin: 0 3rem;
+	white-space: pre-wrap;
+	word-wrap: break-word;
+	border-radius: 2rem;
+	&:focus {
+		outline: 2px solid ${({ theme }) => theme.COLOR.sub};
+	}
+	overflow: auto;
+`
+const InputWrapper = styled.div`
+	width: 99%;
+	height: 12.9vh;
+`
+const IconWrapper = styled.div`
+	position: absolute;
+	bottom: 4rem;
+	right: 9rem;
+`
+const S = {
+	Wrapper,
+	Chatting,
+	ChattingZone,
+	Intro,
+	ChatList,
+	UserImg,
+	ChattingContent,
+	InputChatting,
+	InputWrapper,
+	IconWrapper,
+}
