bc1f61639f9056544b1993105a39bd063e3936d4
taeginote <ab315601@naver.com>
2023-11-22 14:13:24 +0900

    FIX : API수정으로 인한 변경

--- src/Hooks/Queries/get-monthSchedule.ts
+++ src/Hooks/Queries/get-monthSchedule.ts
@@ -17,17 +17,23 @@ interface getMonthScheduleListDataType {
 	}
 }
 
-const getMonthScheduleListData = async (yearAndMonth: string) => {
-	const res = await ScheduleApi.MonthScheduleList(yearAndMonth)
+const getMonthScheduleListData = async (
+	yearAndMonth: string,
+	meetingId: number,
+) => {
+	const res = await ScheduleApi.MonthScheduleList(yearAndMonth, meetingId)
 	return res.data
 }
 
-const useGetMonthScheduleListData = (yearAndMonth: string) => {
+const useGetMonthScheduleListData = (
+	yearAndMonth: string,
+	meetingId: number,
+) => {
 	const { data, isLoading, refetch } = useQuery<
 		getMonthScheduleListDataType,
 		boolean
 	>(['useGetMonthScheduleListData', yearAndMonth], () =>
-		getMonthScheduleListData(yearAndMonth),
+		getMonthScheduleListData(yearAndMonth, meetingId),
 	)
 
 	return { data, isLoading, refetch }
