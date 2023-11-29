372747992f09b6c2f842c7d7ce88530432754104
taeginote <ab315601@naver.com>
2023-11-29 16:32:42 +0900

    DESIGN : 모임활동 디자인 배너

--- src/Pages/Activity/Components/HomePage.tsx
+++ src/Pages/Activity/Components/HomePage.tsx
@@ -1,12 +1,37 @@
 import { styled } from 'styled-components'
 
 function HomePage() {
-	return <S.Wrapper>hello</S.Wrapper>
+	return (
+		<S.Wrapper>
+			<S.ImgWrapper>
+				<S.Img src="/assets/img/activityHome3.png" />
+			</S.ImgWrapper>
+			<S.meetingName>
+				안녕하세요. <span>드림팀</span> 활동 페이지입니다.
+			</S.meetingName>
+		</S.Wrapper>
+	)
 }
 export default HomePage
 
 const Wrapper = styled.div`
-	background-color: yellow;
+	width: 100%;
 `
-
-const S = { Wrapper }
+const Img = styled.img`
+	width: 60%;
+`
+const ImgWrapper = styled.div`
+	width: 100%;
+	background-color: #eab39b;
+	display: flex;
+	justify-content: center;
+`
+const meetingName = styled.div`
+	margin: 2rem;
+	font-size: ${({ theme }) => theme.FONT_SIZE.xslarge};
+	& > span {
+		font-size: ${({ theme }) => theme.FONT_SIZE.xslarge};
+		font-family: ${({ theme }) => theme.FONT_WEIGHT.bold};
+	}
+`
+const S = { Wrapper, Img, ImgWrapper, meetingName }
